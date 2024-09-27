ded8a270fd771348374c8b5abd39052ce72fb6dc
Rick S <stuckey85@gmail.com>
2024-09-27 11:32:06 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ["./PROJECT-2/**/*.{html,js,tsx}"],
   theme: {
     extend: {},
   },
