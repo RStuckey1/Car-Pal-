8a0296789fa0d163a38e4c96f99cedc69453dc90
thunyP <tpt8669@gmail.com>
2024-10-01 19:20:53 -0500

    made changes to model ts added vehicle

--- server/src/models/vehicle.ts
+++ server/src/models/vehicle.ts
@@ -1,25 +1,74 @@
-import { DataTypes, type Sequelize, Model, type Optional } from "sequelize";
-import {}
+import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+import { User } from './user.js';
 
-interface Vehicle {
-    vin: number || string;
-    make: string;
-    model: string;
-    year: number;
-    mileage: number;
-    color: string;
-    price: number;
-    isAvailable: boolean;
+interface IVehicle {
+id: number
+vin: number | string;
+make: string;
+model: string;
+year: number;
+mileage: number;
+color: string;
+price: number;
 }
- 
-interface VehicleCreationAttributes extends Optional<Vehicle, "id"> {}
-export class Vehicle extends Model<Vehicle, VehicleCreationAttributes> implements Vehicle {
-public id!: number;
-public make!: string;
-public model!: string;
-public year!: number;
-public mileage!: number;
-public color!: string;
-public price!: number;
-public isAvailable!: boolean;
+
+interface VehicleCreationAttributes extends Optional<Vehicle, "id"> { }
+
+export class Vehicle extends Model<IVehicle, VehicleCreationAttributes> implements IVehicle {
+    public id!: number
+    public vin!: number;
+    public make!: string;
+    public model!: string;
+    public year!: number;
+    public mileage!: number;
+    public color!: string;
+    public price!: number;
+
+    public readonly assignedUser?: User;
+}
+
+export function VehicleFactory(sequelize: Sequelize): typeof Vehicle {
+    Vehicle.init(
+        { 
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            vin: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            make: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            model: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            year: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            mileage: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            color: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            price: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "vehicles",
+            sequelize,
+        }
+    );
+    return Vehicle;
 }
