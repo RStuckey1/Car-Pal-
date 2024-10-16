debed44bd538f7270f5454e33d22247899f089ae
RStuckey1 <stuckey85@gmail.com>
2024-10-15 23:58:15 -0500

    "renderinbg the data in the table"

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -9,14 +9,15 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_NAME || 'default_db_name',
+    process.env.DB_USER || 'default_db_user',
+    process.env.DB_PW || 'default_db_pw',
     {
       host: 'localhost',
       dialect: 'postgres',
-    },
-  );
+      },
+    );
+  }
 
 
 export default sequelize;
