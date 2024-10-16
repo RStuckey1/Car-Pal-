beaa7742f3ec33407a81f25ad0c881fe36c1779a
RStuckey1 <stuckey85@gmail.com>
2024-10-15 23:37:39 -0500

    connect to postgresql

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,19 +3,20 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+
 
 export default sequelize;
