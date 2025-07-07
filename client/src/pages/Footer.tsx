fce1ce534d4a21937f8295c80be7f1fa0d28d84d
RStuckey1 <stuckey85@gmail.com>
2025-07-07 18:55:49 -0500

    footer correction

--- client/src/pages/Footer.tsx
+++ client/src/pages/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <p>
-      Car-Pal  &copy; {new Date().getFullYear()} Created by Richard Stukey
+      Car-Pal  &copy; {new Date().getFullYear()} Created by Richard Stuckey
       </p>
     </footer>
   );
